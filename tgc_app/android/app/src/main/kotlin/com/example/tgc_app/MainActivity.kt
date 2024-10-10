a8f5bc13d59a8dc8f571045986a5a12fb3b5bad4
Muhammadqodir <m.qodir777@gmail.com>
2024-10-10 16:18:10 +0500

    init commit

---
+++ tgc_app/android/app/src/main/kotlin/com/example/tgc_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tgc_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ tgc_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:tgc_app/pages/main.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TGC',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
